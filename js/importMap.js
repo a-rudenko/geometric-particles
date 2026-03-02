c86ffdfb3bf960f46dc3ebc48c5eb9bb83a76dc0
a-rudenko <i.lex.rudenko@gmail.com>
2026-03-02 12:41:33 +0300

    refactor: update three.js version to 0.183.2.

--- js/importMap.js
+++ js/importMap.js
@@ -1,7 +1,7 @@
 const importMap = {
     "imports": {
-        "three": "https://cdn.jsdelivr.net/npm/three@0.183.1/build/three.module.js",
-        "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.183.1/examples/jsm/"
+        "three": "https://cdn.jsdelivr.net/npm/three@0.183.2/build/three.module.js",
+        "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.183.2/examples/jsm/"
     }
 };
 
